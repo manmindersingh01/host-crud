53cc6ca784d8f9aad5219b334c64b6a3cf4fcbbc
manminder singh <bewithme2407@gmail.com>
2024-07-04 16:32:59 +0530

    build

--- server/server.js
+++ server/server.js
@@ -2,12 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import userRoute from '../server/Routes/user.js';  // Ensure the path and extension are correct
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+
+
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, './client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/index.html'));
+});
+
 app.use('/api/user', userRoute);
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5004;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
